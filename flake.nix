2936dbb749c76ad3b13020a61a4d6b4c668fb1bc
Guilherme Puida Moreira <guilherme@puida.xyz>
2024-06-26 20:46:57 -0300

    format flake with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -2,14 +2,25 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ nixpkgs-fmt hugo ];
+          buildInputs = with pkgs; [
+            nixfmt-rfc-style
+            hugo
+          ];
         };
-      });
+      }
+    );
 }
